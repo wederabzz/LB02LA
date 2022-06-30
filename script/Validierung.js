8434c4489e8de83f9bbe3abf25037b03fad3b03e
Reimann-Lars <84011307+Reimann-Lars@users.noreply.github.com>
2022-06-30 23:15:26 +0200

    push

---
+++ script/Validierung.js
@@ -0,0 +1,104 @@
+
+const form = document.getElementById('form');
+const firstname = document.getElementById('firstname');
+const email = document.getElementById('email');
+const lastname = document.getElementById('lastname');
+const phone = document.getElementById('phone');
+
+// Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
+
+
+// Check phone is valid
+function checkPhone(input) {
+    const re =  /^([0][1-9][0-9](\s|)[0-9][0-9][0-9](\s|)[0-9][0-9](\s|)[0-9][0-9])$|^(([0][0]|\+)[1-9][0-9](\s|)[0-9][0-9](\s|)[0-9][0-9][0-9](\s|)[0-9][0-9](\s|)[0-9][0-9])$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Please enter a valid phone number.');
+        ALL_INPUT_VALID = false;
+    }
+}
+
+
+
+        // Check required fields
+function checkRequired(inputArr) {
+    let isRequired = false;
+    inputArr.forEach(function(input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+            isRequired = true;
+        } else {
+            showSuccess(input);
+        }
+    });
+
+    return isRequired;
+}
+
+// Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} must be at least ${min} characters`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+        );
+    } else {
+        showSuccess(input);
+    }
+}
+
+
+
+
+// Get fieldname
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function validateForm(){
+    if(!checkRequired([firstname, email, lastname, phone])){
+        //Aufgabe: Validierung der Länge für Vorname (2 bis 20) und Nachname (2 bis 50)
+        checkLength(firstname, 3, 15);
+        checkLength(lastname, 6, 25);
+        checkPhone(phone);
+        checkEmail(email);
+
+    }
+}
+
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+    //https://www.w3schools.com/jsref/event_preventdefault.asp
+    e.preventDefault();
+    //First validate form
+    validateForm();
+});
